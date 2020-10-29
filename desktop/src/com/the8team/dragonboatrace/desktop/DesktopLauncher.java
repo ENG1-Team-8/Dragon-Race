82a415b7c8030c57acdc8c967197143ab9e0101c
Matt <mt1346@york.ac.uk>
2020-10-29 14:09:11 +0000

    Create libGDX project

---
+++ desktop/src/com/the8team/dragonboatrace/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.the8team.dragonboatrace.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.the8team.dragonboatrace.DragonBoatRace;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DragonBoatRace(), config);
+	}
+}
