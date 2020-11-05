384516c95577c57fea284181ad7fc5bfc3821288
Charlie Hayes <ch2000@york.ac.uk>
2020-11-05 14:16:40 +0000

    changed some attributes

--- core/src/com/the8team/dragonboatrace/Boat.java
+++ core/src/com/the8team/dragonboatrace/Boat.java
@@ -1,18 +1,15 @@
 package com.the8team.dragonboatrace;
 
 
-//import com.badlogic.gdx.ApplicationAdapter;
-//import com.badlogic.gdx.Gdx;
-//import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-
 
 public abstract class Boat {
     //Boat characterisitcs
-    float mvmntSpeed;
+    int mvmntSpeed;
     int health;
     int stamina;
+    int acceleration;
+    int maneuverability;
 
     //Boat position
     //not sure how this works with the map we are using
@@ -22,7 +19,7 @@ public abstract class Boat {
     //graphics
     Texture sprite;
 
-    public Boat(float mvmntSpeed, int health, int stamina, float mapX, float mapY, float width, float height, Texture sprite){};
+    public Boat(int mvmntSpeed, int health, int stamina, int acceleration, int maneuverability, float mapX, float mapY, float width, float height, Texture sprite){};
 
     public void draw(){}; //not sure what it does or how it works lol
 }
