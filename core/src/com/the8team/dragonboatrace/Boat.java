273e8f195ec18bfcc8ca767d3dc07fa533cc6e9a
Charlie Hayes <ch2000@york.ac.uk>
2020-11-05 15:14:50 +0000

    player boat class created

--- core/src/com/the8team/dragonboatrace/Boat.java
+++ core/src/com/the8team/dragonboatrace/Boat.java
@@ -19,7 +19,20 @@ public abstract class Boat {
     //graphics
     Texture sprite;
 
-    public Boat(int mvmntSpeed, int health, int stamina, int acceleration, int maneuverability, float mapX, float mapY, float width, float height, Texture sprite){};
+    //fastest time
+    float fastestTime;
+
+    public Boat(int mvmntSpeed, int health, int stamina, int acceleration, int maneuverability, float mapX, float mapY, float width, float height, Texture sprite){this.mvmntSpeed = mvmntSpeed;
+        this.health = health;
+        this.stamina = stamina;
+        this.acceleration = acceleration;
+        this.maneuverability = maneuverability;
+        this.mapX = mapX;
+        this.mapY = mapY;
+        this.width = width;
+        this.height = height;
+        this.sprite = sprite;
+    };
 
     public void draw(){}; //not sure what it does or how it works lol
 }
