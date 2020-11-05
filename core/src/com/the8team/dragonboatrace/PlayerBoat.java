9b5bd346e141e003a7d1b6bd41d9f4ad527803bf
Charlie Hayes <ch2000@york.ac.uk>
2020-11-05 15:32:19 +0000

    hello message

--- core/src/com/the8team/dragonboatrace/PlayerBoat.java
+++ core/src/com/the8team/dragonboatrace/PlayerBoat.java
@@ -3,7 +3,7 @@ package com.the8team.dragonboatrace;
 import com.badlogic.gdx.graphics.Texture;
 
 public class PlayerBoat extends Boat{
-
+    // hello
     public PlayerBoat(int mvmntSpeed, int health, int stamina, int acceleration, int maneuverability, float mapX, float mapY, float width, float height, Texture sprite){
         super(mvmntSpeed,health,stamina,acceleration,maneuverability,mapX,mapY,width,height,sprite);
     };
