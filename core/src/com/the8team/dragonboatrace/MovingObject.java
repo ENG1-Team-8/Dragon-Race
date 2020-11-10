4144801ccbead038c78a617460e6c504b226b051
HigherTSC <73180135+HigherTSC@users.noreply.github.com>
2020-11-10 15:18:07 +0000

    Update MovingObject.java

--- core/src/com/the8team/dragonboatrace/MovingObject.java
+++ core/src/com/the8team/dragonboatrace/MovingObject.java
@@ -2,15 +2,16 @@ package com.the8team.dragonboatrace;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.math.Rectangle;
 
-public class MovingObject extends Actor {
+public abstract class MovingObject {
 
     private float xPosition,yPosition;
     private float width,height;
     private Texture objectTexture;
     private float movementSpeed,acceleration,handling;
     private float stamina;
+    private Rectangle BoundBox;
 
     public MovingObject(float xPosition, float yPosition, float width, float height,
                         Texture objectTexture,float movementSpeed, float acceleration, float handling, float stamina)
@@ -24,6 +25,7 @@ public class MovingObject extends Actor {
         this.acceleration = acceleration;
         this.handling = handling;
         this.stamina = stamina;
+        this.BoundBox = new Rectangle(xPosition,yPosition,width,height);
     }
 
     public void draw (Batch batch)
@@ -31,83 +33,44 @@ public class MovingObject extends Actor {
         batch.draw(objectTexture,xPosition,yPosition,width,height);
     }
 
-    @Override
     public float getX() {
-        return super.getX();
+        return xPosition;
     }
 
-    @Override
     public float getY() {
-        return super.getY();
+        return yPosition;
     }
 
-    @Override
     public float getWidth() {
-        return super.getWidth();
+        return width;
     }
 
-    @Override
     public float getHeight() {
-        return super.getHeight();
+        return height;
     }
 
-    public float getMovementSpeed() {
-        return movementSpeed;
-    }
 
-    public float getAcceleration() {
-        return acceleration;
-    }
-
-    public float getHandling() {
-        return handling;
-    }
-
-    public float getStamina() {
-        return stamina;
-    }
-
-    @Override
     public void setX(float x) {
-        super.setX(x);
+        this.xPosition=x;
     }
 
-    @Override
     public void setY(float y) {
-        super.setY(y);
+        this.yPosition=y;
     }
 
-    @Override
-    public void setWidth(float width) {
-        this.width=width;
-    }
-
-    @Override
-    public void setHeight(float height) {
-        this.height=height;
-    }
-
-    @Override
     public void setSize(float width, float height) {
         this.height=height;
         this.width=width;
     }
 
-    @Override
-    public void setBounds(float x, float y, float width, float height) {
-        this.xPosition=x;
-        this.yPosition=y;
-        this.setSize(width,height);
+    public Rectangle getBoundaryBox ()
+    {
+        return BoundBox;
     }
 
     public void setMovementSpeed (float movementSpeed)
     {
         this.movementSpeed=movementSpeed;
-    }
-
-    public void setStamina (float stamina)
-    {
-        this.stamina=stamina;
     }
 
 }
