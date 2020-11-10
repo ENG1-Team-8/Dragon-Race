ba6c09628f2e7db430d3f155feb25b4e956d8f83
Josh <js2961@york.ac.uk>
2020-11-10 14:46:56 +0000

    Added class files

--- core/src/com/the8team/Obstacle.java
+++ core/src/com/the8team/Obstacle.java
@@ -1,5 +1,16 @@
 public class Obstacle extends Moving_object {
 
+    int damage_dealt;
+    int health;
+    Texture img;
+
+    public Obstacle(int damage_dealt, int health, Texture img) {
+
+        this.damage_dealt = damage_dealt;
+        this.health = health;
+        
+
+    }
 
 
 }
