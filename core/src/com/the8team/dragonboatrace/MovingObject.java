909e0cc1d06e32abfa50e9fc1da61e377f0a8b9c
HigherTSC <73180135+HigherTSC@users.noreply.github.com>
2020-11-10 15:27:38 +0000

    Update MovingObject.java

--- core/src/com/the8team/dragonboatrace/MovingObject.java
+++ core/src/com/the8team/dragonboatrace/MovingObject.java
@@ -2,7 +2,6 @@ package com.the8team.dragonboatrace;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.math.Rectangle;
 
 public abstract class MovingObject {
 
