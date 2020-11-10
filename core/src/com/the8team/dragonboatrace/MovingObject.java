ce983137315cfb9276e6c21a1d216ce1dd09a8e2
HigherTSC <73180135+HigherTSC@users.noreply.github.com>
2020-11-10 15:27:24 +0000

    Update MovingObject.java

--- core/src/com/the8team/dragonboatrace/MovingObject.java
+++ core/src/com/the8team/dragonboatrace/MovingObject.java
@@ -9,12 +9,10 @@ public abstract class MovingObject {
     private float xPosition,yPosition;
     private float width,height;
     private Texture objectTexture;
-    private float movementSpeed,acceleration,handling;
-    private float stamina;
-    private Rectangle BoundBox;
+    private float movementSpeed,acceleration;
 
     public MovingObject(float xPosition, float yPosition, float width, float height,
-                        Texture objectTexture,float movementSpeed, float acceleration, float handling, float stamina)
+                        Texture objectTexture,float movementSpeed)
     {
         this.xPosition = xPosition;
         this.yPosition = yPosition;
@@ -22,20 +20,11 @@ public abstract class MovingObject {
         this.height = height;
         this.objectTexture = objectTexture;
         this.movementSpeed = movementSpeed;
-        this.acceleration = acceleration;
-        this.handling = handling;
-        this.stamina = stamina;
-        this.BoundBox = new Rectangle(xPosition,yPosition,width,height);
     }
 
     public void draw (Batch batch)
     {
         batch.draw(objectTexture,xPosition,yPosition,width,height);
-    }
-
-    public void setMovementSpeed (float movementSpeed)
-    {
-        this.movementSpeed=movementSpeed;
     }
 
 }
